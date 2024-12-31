9629d69f14ae320f026218e2d2df44d7c4ed6ae0
Bob Wint <36092563+Bobwint@users.noreply.github.com>
2024-12-31 13:28:05 -0500

    Add files via upload
    Added a frontend directory to all my games

---
+++ tower-defense-github/js/listeners.js
@@ -0,0 +1,46 @@
+// -------------------
+// --Event Listeners--
+// -------------------
+const mouse = {
+  x: undefined,
+  y: undefined
+}
+
+canvas.addEventListener('click', (event) => {
+  if (activeTile && !activeTile.isOccupied && coins - buildingCoinValue >= 0) {
+    coins -= buildingCoinValue
+    document.querySelector('#coinCounter').innerHTML = coins
+    buildings.push(
+      new Building({
+        position: {
+          x: activeTile.position.x,
+          y: activeTile.position.y
+        }
+      })
+    )
+    activeTile.isOccupied = true
+    // Sort the buildings array top-down to avoid building image perspective issues (i.e. A building added behind an existing building (i.e. added 1 tile higher) will actually display in front of the building below, which is not what is desired, sort the array top-down to avoid this)
+    buildings.sort((a, b) => {
+      return a.position.y - b.position.y
+    })
+  }
+})
+
+window.addEventListener('mousemove', (event) => {
+  mouse.x = event.clientX
+  mouse.y = event.clientY
+
+  activeTile = null
+  for (let i = 0; i < placementTiles.length; i++) {
+    const tile = placementTiles[i]
+    if (
+      mouse.x > tile.position.x &&
+      mouse.x < tile.position.x + tile.size &&
+      mouse.y > tile.position.y &&
+      mouse.y < tile.position.y + tile.size
+    ) {
+      activeTile = tile
+      break
+    }
+  }
+})
